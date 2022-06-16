08d72f5375ab1dc61fa3328648f0e198b399b7ee
Yogesh <yogeshyadavas2@gmail.com>
2022-06-16 12:09:41 +0530

    initial commit

---
+++ Print.java
@@ -0,0 +1,8 @@
+public class Print {
+
+    public static void main(String[] args) {
+        System.out.println("Yogesh Yadav");
+        System.out.println("Yogesh Yadav");
+}
+
+}
---
+++ Yogesh.java
@@ -0,0 +1,5 @@
+class Yogesh {
+    public static void main(String[] args) {
+        System.out.println("Yogesh Yadav");
+    }
+}
